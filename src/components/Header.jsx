a3103412b290d6fe0dfefe49f34469747ebe5ae3
Mikal-E <melamin101@gmail.com>
2026-07-15 11:43:23 -0400

    Created the header component.

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+/* Header component for "Employee Dirctory" */
+
+function Header() {
+
+    return (
+
+        <div className="Header">
+
+            <h1>Employee Directory</h1>
+
+        </div>
+
+    )
+
+}
+
+export default Header
